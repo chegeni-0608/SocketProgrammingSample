4f2969102bf4f2addfe5b4834cdfa21b1c76f352
mmoa8063@gmail.com <mmoa8063@gmail.com>
2024-09-27 00:50:00 +0330

    Add project files.

---
+++ SenderClient/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SenderClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSendWithSocket = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 139);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnSendWithSocket
+            // 
+            this.btnSendWithSocket.Location = new System.Drawing.Point(12, 178);
+            this.btnSendWithSocket.Name = "btnSendWithSocket";
+            this.btnSendWithSocket.Size = new System.Drawing.Size(115, 23);
+            this.btnSendWithSocket.TabIndex = 1;
+            this.btnSendWithSocket.Text = "Send With Socket";
+            this.btnSendWithSocket.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(457, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Log :";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(460, 78);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(207, 321);
+            this.txtLog.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSendWithSocket);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSendWithSocket;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ SocketProgrammingSample/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SocketProgrammingSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartListener = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReceiveMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartListener
+            // 
+            this.btnStartListener.Location = new System.Drawing.Point(61, 52);
+            this.btnStartListener.Name = "btnStartListener";
+            this.btnStartListener.Size = new System.Drawing.Size(160, 23);
+            this.btnStartListener.TabIndex = 0;
+            this.btnStartListener.Text = "Start Listener";
+            this.btnStartListener.UseVisualStyleBackColor = true;
+            this.btnStartListener.Click += new System.EventHandler(this.btnStartListener_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Received Message :";
+            // 
+            // txtReceiveMessage
+            // 
+            this.txtReceiveMessage.Location = new System.Drawing.Point(66, 132);
+            this.txtReceiveMessage.Multiline = true;
+            this.txtReceiveMessage.Name = "txtReceiveMessage";
+            this.txtReceiveMessage.Size = new System.Drawing.Size(599, 287);
+            this.txtReceiveMessage.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtReceiveMessage);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStartListener);
+            this.Name = "Form1";
+            this.Text = "Receiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartListener;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtReceiveMessage;
+    }
+}
+
